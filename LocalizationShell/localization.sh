5be95f6c94440ab7cbc458206a7a04898d1a40a5
shuye <shuye@mail.com>
2020-04-13 12:11:36 +0800

    1. Update content.

---
+++ LocalizationShell/localization.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+################################## 配置相关 #################################
+# 多语言表格
+xlsx[0]=$(find . -iname "*default*.xlsx")
+xlsx[1]=$(find . -iname "*sub*.xlsx")
+
+# 多语言文件名称
+LOCALIZABLE_FILES[0]="Localizable.strings"
+LOCALIZABLE_FILES[1]="StyleProject.strings"
+
+# 多语言存放的目录名字
+LOCALIZABLE_DIR_NAME="Localization"
+
+# 自定义的换行符
+NEWLINE="#@@#"
+
+################################## 定义常量或变量 #################################
+# =============================== 路径相关 =============================== #
+# 当前目录路径
+CURRENT_DIR=$(cd "$(dirname "$0")"; pwd)
+PROJECT_DIR=$(cd "${CURRENT_DIR}/.."; pwd)
+
+# Localization目录
+LOCALIZATION_PATH=$(cd "${PROJECT_DIR}"; cd "$(find . -name "${LOCALIZABLE_DIR_NAME}" -type d)"; pwd)
+
+# =============================== 文件相关 =============================== #
+CSV_FILE="tmp.csv"
+
+################################## 开始执行内容 ##################################
+
+for ((i=0; i<${#xlsx[@]}; i++))
+do
+
+current_xlsx=${xlsx[$i]}
+current_localizable_file=${LOCALIZABLE_FILES[$i]}
+
+# 先将xlsx转换成csv，确保安装了python的插件，https://github.com/dilshod/xlsx2csv
+xlsx2csv -d 'tab' $current_xlsx "$CSV_FILE"
+
+csv_content=`cat $CSV_FILE`
+
+# 获取当前所有列数、行数
+allColumn=`echo "$csv_content" | awk -F'\t' 'END{print NF}'`
+allRow=`echo "$csv_content" | awk -F't' 'END{print NR}'`
+
+echo -e "总共有${allColumn}列，${allRow}行\n"
+
+# 从第3列开始循环，将每一列多语言写入对应语言的文件中
+for ((j=3; j<=$allColumn; j++))
+do
+
+# 第一行都是各国的语言代码
+language_code=`echo "$csv_content" | awk -F'\t' -v awk_var="$j" '{ if (NR==1) {print $awk_var;}}'`
+#echo "当前处理的语言: $language_code"
+# 1. 读取转换的csv文件的内容
+# 2. awk命令将内容按'tab'为分隔符，将内容分隔
+# 3. 如果是第一行，则添加注释，否则，按"xxx"="xxx";的格式化输出到content中, 如果是空列，则将第二列内容作为模块分类进行注释。
+content=`echo "$csv_content" | awk -F'\t' -v awk_var="$j" '{ if (NR==1) {printf "// %s;\n", $awk_var;} else { keys[NR]=$1; desc[NR]=$2; if ($awk_var == "") { printf "// %s\n", desc[NR]; } else { printf "\"%s\"=\"%s\";\n", keys[NR], $awk_var } } }'`
+
+# 替换
+content=$(echo "$content" | sed "s/${NEWLINE}/\\\n/g")
+
+echo "$content"
+
+file="${LOCALIZATION_PATH}/${language_code}.lproj/$current_localizable_file"
+
+# 判断是否存在多语言文件
+if [ -f "$file" ]; then
+    echo "$content" > $file
+else
+    echo "❗项目没有设置${language_code}的${current_localizable_file}多语言文件"
+fi
+
+done
+
+# 最后删除csv文件
+rm -f $CSV_FILE
+
+done
+
+echo "完成操作!"
+
--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # localizationshell
-一键配置iOS项目的多语言内容
+这个shell脚本主要为是了解决iOS项目中多语言内容设置机械重复的问题，在此之前基本上是开者发根据运营或者产品给的excel文件，手动一个一个内容复制到多语言的.strings文件中。也曾尝试找一些开源工具，但都没有找到一个合适的。借着之前有用过shell编写iOS打包脚本的经验，基于shell写一个工具，来解放开发者的生产力。
+
+## 一些约定
+
+虽然能很大程序上解决生产力的问题，但shell脚本还是有自身的局限性，不像cocoapods等一些ruby或者python编写的一些工具，所以在使用这个脚本的时候需要你做一些约定的东西。
+
+1. 首先假定你是有 shell 的基础知识，因为要适合各个不同的项目，或者你自定义的一些内容，可能需要你在这个脚本中做一点修改；
+
+2. 该脚本依赖一个开源工具（https://github.com/dilshod/xlsx2csv），是将excel表格转换成csv文件的，所以使用之前，请先安装这个工具；
+
+3. 假设你的项目结构大概是这样子的
+
+   ```
+   Project_dir
+   ├── ......
+   ├── shell dir(这里是存放这个shell的文件夹)
+   ├── your project name.xcodeproj
+   └── your project name
+   	└── Localzation 
+   		├── en.lproj
+   		├── zh-Hans.lproj
+   		├── ......
+   		└── ar.lproj
+   			├── a.strings
+   			├── b.strings
+   			├── ....
+   			└── xxx.strings
+   ```
+
+4. 假设你的excel表格是如下格式
+
+   | key(.strings文件中的key) | 标题(文案对应位置) | en   | zh-Hans | ja   | ko   | ...  | ar   |
+   | ------------------------ | ------------------ | ---- | ------- | ---- | ---- | ---- | ---- |
+   | key1                     | 图标               | xx   | xx      | xx   | xx   | xx   | xx   |
+   | key2                     | 按钮               | xx   | xx      | xx   | xx   | xx   | xx   |
+   | key3                     | 标题               | xx   | xx      | xx   | xx   | xx   | xx   |
+   | key4                     | 副标题             | xx   | xx      | xx   | xx   | xx   | xx   |
+
+5. 由于存在多个excel或者以前对应有多个.strings文件，所以对文件名也要和shell脚本里的对应，你可以自行定义规则，然后改写"配置相关"一栏里的配置；
+
+6. excel内容中原则上建议一行一个文案，如果一个位置的文案一定要用到换行，则建议和编写excel的人员协定好，使用其他符号替换换行，然后在shell脚本中`NEWLINE=xxx`一行改成你想要的符号，这里默认是`#@@#`。
+
+
+
+## 使用
+
+```shell
+$ ./localization.sh
+```
+
+如果提示没有权限，则先使用 `chmod` 修改一下权限。
+
+## 最后
+
+虽然上面提到一些约定，当然如果你也有shell的经验，完全可以自己在这上面做修改，以达到符合你的业务。或者，你有比这个更好的工具，请分享给我，谢谢啦~
+
